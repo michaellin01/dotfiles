1b1b192130f6fe59f58cf093ee46a315c3944114
Michael Lin <michaellin01@gmail.com>
2023-06-08 19:28:27 -0400

    added powerlevel10k theme

--- link.sh
+++ link.sh
@@ -10,3 +10,6 @@ mkdir -p ~/.config
 ln -sf ${BASEDIR}/nvim ~/.config/nvim
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+ln -sf ${BASEDIR}/p10k.zsh ~/.p10k.zsh
+nvim --headless +PlugInstall +qall
