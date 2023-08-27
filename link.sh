e3857a9e8986ab3818e4e9954151d003a2a44924
Michael Lin <michaellin01@gmail.com>
2023-08-27 00:56:34 -0400

    added rg install

--- link.sh
+++ link.sh
@@ -9,3 +9,5 @@ mkdir -p ~/.config
 ln -sf ${BASEDIR}/nvim ~/.config/nvim
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+apt install rg
